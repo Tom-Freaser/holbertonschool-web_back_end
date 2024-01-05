d42463fb0bde93432b3736a4657f28d3b03b451d
root <root@yassin>
2024-01-05 18:30:44 +0100

    async await function

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 """
-basics of async
+Use the random module.
 """
-import asyncio
 import random
+import asyncio
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    """Await for random seconds
+    then return amount of seconds awaited
+
+    Parameters
+    ----------
+    max_delay : int
+        max delaying(sleep) time in second(s)
+
+    Returns
+    -------
+    float
+        second(s) as float number
     """
-    waits for a random delay between 0 & "max_delay" seconds then returns it
-    """
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
+    seconds = random.uniform(0, max_delay)
+    await asyncio.sleep(seconds)
+    return seconds
